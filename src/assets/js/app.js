c7b92555305df518d1698022285045b75abffda5
walimbubuyog <walimbubuyog@apdgroup.com>
2016-12-02 15:43:18 +0800

    setup of sidebar pages

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,9 @@
 $(document).foundation();
+
+//overrides the foundation accordion, making the accordion title redirect to a page
+$('#sidebar-accordion .no-sub-item').on('click', function(e){
+	e.preventDefault();
+
+	var url = $(this).attr('href');
+	window.location.href = url;
+})
