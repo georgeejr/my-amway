98448c644bc224d452ed184faf458701663446be
George <geoash16@hotmail.com>
2016-12-05 11:14:11 +0800

    update

---
+++ src/assets/js/nav.js
@@ -0,0 +1,19 @@
+/*
+Author: georgeejr
+Dependencies: jQuery 
+*/
+var awApp = awApp || {};
+
+(function($){
+    'use strict';
+      var nav = {
+            init: () =>{
+                     
+            } 
+        };
+      $(function(){
+            nav.init();
+      });
+
+
+})(jQuery);
